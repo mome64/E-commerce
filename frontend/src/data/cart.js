0c45702a113f7cb8491cac1fec724db6f3af8c80
Bamsi <mesoudmohammed393@gmail.com>
2025-01-16 19:16:06 +0300

    Your descriptive commit message

---
+++ frontend/src/data/cart.js
@@ -0,0 +1,50 @@
+export let cart = [
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 1,
+    },
+    {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 2,
+    }
+]
+
+export function addToCart (productId) {
+    let matchingItem
+    cart.forEach((item) => {
+        if (productId === item.productId)
+        {
+            matchingItem = item
+        }
+    })
+
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${ productId }`)
+    const quantity = Number(quantitySelector.value)
+
+    if (matchingItem)
+    {
+        matchingItem.quantity += quantity
+    }
+    else
+    {
+        cart.push({
+            // productId: productId,
+            // quantity: quantity
+            productId,
+            quantity
+        })
+    }
+}
+
+export function removeFromCart (productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+
+        if (cartItem.id !== productId)
+        {
+            newCart.push(cartItem);
+        }
+    });
+    cart = newCart;
+}
